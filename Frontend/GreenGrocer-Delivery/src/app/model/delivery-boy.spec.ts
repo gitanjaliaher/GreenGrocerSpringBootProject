859ce81e9d49a1558f2da69aba20226193cbb0cd
gitanjaliaher <157561189+gitanjaliaher@users.noreply.github.com>
2024-03-21 08:56:57 +0530

    Delete Frontend/GreenGrocer-Delivery/src directory

--- Frontend/GreenGrocer-Delivery/src/app/model/delivery-boy.spec.ts
+++
@@ -1,7 +0,0 @@
-import { DeliveryBoy } from './delivery-boy';
-
-describe('DeliveryBoy', () => {
-  it('should create an instance', () => {
-    expect(new DeliveryBoy()).toBeTruthy();
-  });
-});
--- Frontend/GreenGrocer-Delivery/src/app/model/delivery-boy.ts
+++
@@ -1,9 +0,0 @@
-export class DeliveryBoy {
-    deliveryBoyId : number | undefined;
-    deliveryBoyName : number | undefined;
-    deliveryBoyAddress : number | undefined;
-    deliveryBoyCity : number | undefined;
-    deliveryBoyEmail : number | undefined;
-    deliveyBoyContact : number | undefined;
-    deliveryBoyPassword : number | undefined;
-}
